7cf5773db126efbb575f3babd2ffb571233a8696
Bright Amoateng <bright.amoateng150@gmail.com>
2021-09-11 18:29:35 -0400

    Send an email using nodemailer

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ Install the necessary packages from npm:
 npm install
 ```
 
+Create a `.env` file using the existing `.env.example` and modify where necessary.\
+Access to an email account is required. Make sure 3rd party apps (such as this one)
+can access the account (settings may have to be changed).
+
 Run the app:
 ```
 npm start
---
+++ server.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const hbs = require("nodemailer-express-handlebars");
+const nodemailer = require("nodemailer");
+const path = require("path");
+
+const { EMAIL_SERVICE, EMAIL_USER, EMAIL_PASS } = process.env;
+
+// initialize nodemailer
+let transporter = nodemailer.createTransport({
+    service: EMAIL_SERVICE,
+    auth: {
+        user: EMAIL_USER,
+        pass: EMAIL_PASS
+    }
+});
+
+// point to the template folder
+const handlebarOptions = {
+    viewEngine: {
+        partialsDir: path.resolve("./views/"),
+        defaultLayout: false,
+    },
+    viewPath: path.resolve("./views/")
+};
+
+// use a template file with nodemailer
+transporter.use("compile", hbs(handlebarOptions));
+
+let mailOptions = {
+    from: `"BrightMailer" <${EMAIL_USER}>`, // sender address
+    to: `${EMAIL_USER}`, // list of receiveers
+    subject: "Welcome!",
+    template: "email", // the name of the template file i.e email.handlebars
+    context: {
+        name: "Random Person",
+        company: "BrightMailer"
+    }
+};
+
+// trigger the sending of the email
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+
+    console.log("Message sent: " + info.response);
+});
